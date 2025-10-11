abbde35407b83bed341415aa8335946ffdd114be
kaiqueSantosGi) <kaiqueg.sant@gmail.com>
2025-10-11 00:46:41 -0300

    Inicio backend (Login e cadastro) & configuração inicial Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const db = require("./database.js");
+const routes = require("./routes.js"); // Importa nosso novo arquivo de rotas
+
 const app = express();
-const PORT = 3333; // Define a porta do nosso servidor
+const PORT = 3333;
 
-// Middleware para permitir que o servidor entenda JSON
+// ===== NOVAS CONFIGURAÇÕES =====
+app.use(cors()); // Habilita o CORS para todas as rotas
 app.use(express.json());
+app.use("/api", routes); // Diz ao Express para usar nossas rotas com o prefixo /api
+// ===============================
 
-// Uma rota de teste para verificar se o servidor está no arS
+// Rota de teste (pode manter ou remover)
 app.get("/", (request, response) => {
   response.json({ message: "Backend do CantinMoci está online!" });
 });
 
-// Inicia o servidor e fica "ouvindo" por requisições
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
